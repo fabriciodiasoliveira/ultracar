505f2813e219cd1c367f4d114899883da7a499f2
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2023-10-16 15:55:14 -0300

    Commit inicial

---
+++ src/main/java/com/fabricio/ultracar/UltracarApplication.java
@@ -0,0 +1,13 @@
+package com.fabricio.ultracar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltracarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltracarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabricio/ultracar/UltracarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricio.ultracar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltracarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
